5643bb3eb04a94a6d3068c357dfb7a880f967512
test <zjp15083791743@163.com>
2020-02-27 20:25:23 +0800

    as ds

--- pull/ViewController.m
+++ pull/ViewController.m
@@ -1,10 +1,5 @@
-//
-//  ViewController.m
-//  pull
-//
-//  Created by test on 2020/2/27.
-//  Copyright © 2020 test. All rights reserved.
-//
+
+
 
 #import "ViewController.h"
 
@@ -17,6 +12,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     //我是傻逼
+    //我是新加的傻逼
+    
 }
 
 
